97f6f11cf5a8ecc01b59ec22d16ee3777e135ce0
uskudu <usskudu@gmail.com>
2026-02-25 15:27:24 +0300

    log done

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,8 +1,25 @@
 package main
 
-func main() {
-	// todo: init config: cleanenv
+import (
+	"log/slog"
+	"os"
+	"sptringTresRestAPI/internal/config"
+)
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+
+	log.Debug("debug messages are enabled")
 	// todo: init logger: slog
 
 	// todo: init storage: sqlite, postgresql
@@ -11,4 +28,33 @@ func main() {
 
 	// todo: init server:
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	}
+	return log
 }
