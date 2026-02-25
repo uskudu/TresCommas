b0779d9fa9cf1e3d754eb59b5a84260ee7e52708
uskudu <usskudu@gmail.com>
2026-02-25 14:44:17 +0300

    init

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// todo: init config cleanenv
+
+	// todo: init logger
+
+	// todo: init storage
+
+	// todo: init router
+
+	// todo: init server
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module sptringTresRestAPI
+
+go 1.24
